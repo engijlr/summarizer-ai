687b2153e68f48a3d0d3844efdf23ddfcc0ec2e3
Engi Lozada <engilozada@gmail.com>
2023-06-16 23:45:45 +0200

    Adding state to the input

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,12 +3,18 @@ import { useState, useEffect } from "react";
 import { copy, linkIcon, loader, tick } from "../assets";
 
 const Demo = () => {
+  const [article, setArticle] = useState({ url: "", summary: "" });
+
+  const handleSubmit = async (e) => {
+    alert("submitted");
+  };
+
   return (
     <section className="mt-16 w-full max-w-xl">
       <div className="flex flex-col w-full gap-2">
         <form
           className="relative flex justify-center items-center"
-          onSubmit={() => {}}
+          onSubmit={handleSubmit}
         >
           <img
             src={linkIcon}
@@ -18,8 +24,8 @@ const Demo = () => {
           <input
             type="url"
             placeholder="Enter a URL"
-            value=""
-            onChange={() => {}}
+            value={article.url}
+            onChange={(e) => setArticle({ ...article, url: e.target.value })}
             required
             className="url_input peer"
           />
@@ -30,7 +36,10 @@ const Demo = () => {
             <p>↵</p>
           </button>
         </form>
+        {/* Urls History*/}
       </div>
+
+      {/*Results */}
     </section>
   );
 };
