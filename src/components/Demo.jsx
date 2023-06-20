8eb641ace207b76abacbb334ac9071cb7ffc1538
Engi Lozada <engilozada@gmail.com>
2023-06-20 21:22:29 +0200

    Adding url history

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -29,9 +29,10 @@ const Demo = () => {
       const updatedAllArticles = [newArticle, ...allArticles];
 
       setArticle(newArticle);
+      console.log(newArticle);
       setAllArticles(updatedAllArticles);
 
-      console.log(newArticle);
+      localStorage.setItem("articles", JSON.stringify(updatedAllArticles));
     }
   };
 
@@ -63,6 +64,26 @@ const Demo = () => {
           </button>
         </form>
         {/* Urls History*/}
+        <div className="flex flex-col gap-1 max-h-60 overflow-y-auto">
+          {allArticles.map((item, index) => (
+            <div
+              key={`link-${index}`}
+              onClick={() => setArticle(item)}
+              className="link_card"
+            >
+              <div className="copy_btn">
+                <img
+                  src={copy}
+                  alt="copy_icon"
+                  className="w-[40%] h-[40%] object-contain"
+                />
+              </div>
+              <p className="flex-1 font-satoshi text-blue-700 font-medium text-sm truncate">
+                {item.url}
+              </p>
+            </div>
+          ))}
+        </div>
       </div>
 
       {/*Results */}
